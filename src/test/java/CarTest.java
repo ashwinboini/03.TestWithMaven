c4f2892fd01136cb5ab499f82a824aeef88b89cc
Ashwin Boini <ashwinboini@yahoo.com>
2017-07-14 15:14:59 -0400

    check in with sql delete

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,17 +1,19 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 import com.fanniemae.payroll.dao.CarRepository;
 
 public class CarTest {
 
-	
+	@Test
 	public void testSizeFor10Items() {
 
 		CarRepository carRep = new CarRepository();
 		
 		int size = carRep.findMaps().size();
 		
-		assertTrue("Size is: " + size, size ==3);
+		assertTrue("Size is: " + size, size ==20);
 	}
 
 }
